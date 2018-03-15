c9606f45549f60e4c9467490dfd0583547077848
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-15 22:10:29 +0100

    Add tests for validation code

--- chpasswd/doveadmTest.ml
+++ chpasswd/doveadmTest.ml
@@ -9,6 +9,9 @@ let make_equal_result eqok eqfail prok prfail =
 let equal_string_result =
   make_equal_result (=) (=) (fun s -> "ok: " ^ s) (fun s -> "failure: " ^ s)
 
+let result_failure ?msg result =
+  Assertion.is_true ?msg (match result with Ok _ -> false | Error _ -> true)
+
 let test_password_encode_1 =
   Test.make_simple_test ~title:"Encode a password with doveadm"
     (fun () ->
@@ -16,9 +19,31 @@ let test_password_encode_1 =
 	 (Ok "{CRAM-MD5}6c872c7dd7cdf68f9392efce0ac212b1e361b8ce9acc5cf286a978b14755ec2d")
 	 (Doveadm.password_encode ~user:"foo" ~pass:"bar"))
 
+let test_password_encode_2 =
+  Test.make_simple_test ~title:"Try to encode a password with a line break"
+    (fun () ->
+       result_failure ~msg:"Expected failure"
+	 (Doveadm.password_encode ~user:"foo" ~pass:"bar\n"))
+
+let test_password_encode_3 =
+  Test.make_simple_test ~title:"Try to encode a password; user name has a line break"
+    (fun () ->
+       result_failure ~msg:"Expected failure"
+	 (Doveadm.password_encode ~user:"foo\n" ~pass:"bar"))
+
+let test_password_encode_4 =
+  Test.make_simple_test ~title:"Try to encode a password; user name needs encoding"
+    (fun () ->
+       equal_string_result
+	 (Ok "{CRAM-MD5}6c872c7dd7cdf68f9392efce0ac212b1e361b8ce9acc5cf286a978b14755ec2d")
+  (Doveadm.password_encode ~user:"foo'; exit 1 #" ~pass:"bar"))
+
 (* Tests for auth are surprisingly difficult to set up, don't do it here. *)
 
 let () =
   Test.run_tests [
-    test_password_encode_1
+    test_password_encode_1;
+    test_password_encode_2;
+    test_password_encode_3;
+    test_password_encode_4;
   ]
