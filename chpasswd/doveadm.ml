1e5f199ea07f94f33c9b20412123015d5c955152
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-15 20:21:57 +0100

    Interface to doveadm - password encoder.
    Use the doveadm utility to do password encoding for us. This is
    the most future-proof version since the dovecot library does not
    expose this functionality.

---
+++ chpasswd/doveadm.ml
@@ -0,0 +1,21 @@
+let password_encode user pass =
+  let cmdline = Format.sprintf "doveadm pw -u '%s'" user in
+  let (cout, cin, cerr) = Unix.open_process_full cmdline [| |] in
+  try
+    for i = 1 to 2 do
+      output_string cin (pass ^ "\n")
+    done;
+    flush cin;
+    let result = try Ok (input_line cout) with End_of_file -> Error (input_line cerr)
+    in match Unix.close_process_full (cout, cin, cerr) with
+    | Unix.WEXITED 0 -> result
+    | Unix.WSIGNALED signal | Unix.WSTOPPED signal ->
+      Error (Format.sprintf "doveadm killed by signal %d" signal)
+    | Unix.WEXITED status -> match result with
+      | Ok _ -> Error (Format.sprintf "doveadm exited with %d" status)
+      | Error e -> Error (Format.sprintf "doveadm exited with %d, %s" status e)
+  with e -> ignore (Unix.close_process_full (cout, cin, cerr)); raise e
+
+
+
+
---
+++ chpasswd/doveadmTest.ml
@@ -0,0 +1,23 @@
+open Kaputt
+
+
+let make_equal_result eqok eqfail prok prfail =
+  Assertion.make_equal (fun r1 r2 -> match r1, r2 with
+      | Ok o1, Ok o2 -> eqok o1 o2
+      | Error e1, Error e2 -> eqfail e1 e2
+      | _, _ -> false)
+    (function Ok o -> prok o | Error e -> prfail e)
+let equal_string_result =
+  make_equal_result (=) (=) (fun s -> "ok: " ^ s) (fun s -> "failure: " ^ s)
+
+let test_password_encode_1 =
+  Test.make_simple_test ~title:"Encode a password with doveadm"
+    (fun () ->
+       equal_string_result
+	 (Ok "{CRAM-MD5}6c872c7dd7cdf68f9392efce0ac212b1e361b8ce9acc5cf286a978b14755ec2d")
+	 (Doveadm.password_encode "foo" "bar"))
+
+let () =
+  Test.run_tests [
+    test_password_encode_1
+  ]
