0c679890adbb297a7ea9a9414da2626295941198
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-16 14:44:03 +0100

    Generate sufficiently random tokens

--- chpasswd/token.ml
+++ chpasswd/token.ml
@@ -1,1 +1,5 @@
-let generate (): string = failwith "Not implemented!"
+let generate () =
+  let open Cryptokit in
+  let rand = Random.string Random.secure_rng 64
+  and time = Time.current_string ()
+  in hash_string (Hash.sha512 ()) (rand ^ time)
