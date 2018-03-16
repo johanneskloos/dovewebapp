fc28745c15865d9559bbaccfd3631e0098722620
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-16 14:22:38 +0100

    Mail sending

---
+++ chpasswd/mails.ml
@@ -0,0 +1,19 @@
+let send_mail subject template ~email ~token =
+  let models = [
+    ("mail", Jg_types.Tstr email);
+    ("token", Jg_types.Tstr token)
+  ] in let message = Netsendmail.compose
+      ~from_addr:("Account management",
+		  "no-reply@" ^ !Config.domain)
+      ~to_addrs:[("", email)]
+      ~subject
+      (Template.from_file ~models template)
+  in ignore message
+
+let send_token_email db ~user ~token =
+  let email = match Model.user_get_email db user with
+    | Some address -> address
+    | None -> user ^ "@" ^ !Config.domain
+  in send_mail "Forgotten password" "forgot.822" ~email ~token
+
+let send_account_email = send_mail "Your new account" "new.822"
