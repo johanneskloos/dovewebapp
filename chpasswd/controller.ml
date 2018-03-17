d97bad6b72b1eb6c5c00e721d5ade9806f95152e
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-17 12:12:21 +0100

    Validate inputs

--- chpasswd/controller.ml
+++ chpasswd/controller.ml
@@ -2,6 +2,77 @@ open View
 
 let cookie_name = "session"
 
+let validate_string pred str =
+  try
+    for i = 0 to String.length str - 1 do
+      if not (pred (String.unsafe_get str i)) then raise Exit
+    done;
+    true
+  with Exit -> false
+
+(* Valid user names follow the RFC2822 dot-atom syntax.
+   We are a bit more generous than RFC2822 here and allow
+   multiple '.' characters in a row. *)
+let valid_user_chars = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9'
+  | '!' | '#' | '$' | '%' | '&' | '\'' | '*' | '+' | '-' | '/' | '='
+  | '?' | '^' | '_' | '`' | '{' | '|' | '}' | '~' | '.' -> true
+  | _ -> false
+
+let validate_user str =
+  str <> "" && validate_string valid_user_chars str
+
+(* Passwords can have almost any character in them, except
+   line breaks and NUL. *)
+let valid_pass_chars = function
+  | '\x00' | '\r' | '\n' -> false
+  | _ -> true
+
+let validate_pass str = validate_string valid_pass_chars str
+
+let validate_email str =
+  match Netaddress.parse str with
+  | [`Mailbox _] -> true (* We could parse a single e-mail address *)
+  | _ -> false
+  | exception _ -> false
+
+exception InvalidUser of string
+exception InvalidAddress of string
+exception InvalidPass
+exception ArgumentMissing of string
+exception ArgumentEmpty of string
+
+let get_user (cgi: Netcgi.cgi) field =
+  try
+    let user = (cgi # argument field) # value in
+    if validate_user user then user else raise (InvalidUser user)
+  with Not_found -> raise (ArgumentMissing field)
+
+let get_nonempty_string_option (cgi: Netcgi.cgi) field =
+  try
+    let value = (cgi # argument field) # value in
+    if value = "" then None else Some value
+  with Not_found -> raise (ArgumentMissing field)
+
+let get_nonempty_string cgi field =
+  match get_nonempty_string_option cgi field with
+  | Some value -> value
+  | None -> raise (ArgumentEmpty field)
+
+let get_mail_option (cgi: Netcgi.cgi) field =
+  match get_nonempty_string_option cgi field with
+  | Some mail ->
+    if validate_email mail then Some mail else raise (InvalidAddress mail)
+  | None -> None
+
+let get_pass cgi field =
+  let pass = get_nonempty_string cgi field in
+  if validate_pass pass then pass else raise InvalidPass
+
+let get_admin (cgi: Netcgi.cgi) field =
+  let open Model in
+  if cgi # argument_exists field then Admin else User
+
 let event_login_login db user pass (cgi: Netcgi.cgi) =
   match Model.session_login db ~user ~pass with
   | None -> view_login db LoginFailed
@@ -21,8 +92,8 @@ let event_login db (cgi: Netcgi.cgi) =
   match cgi#argument_value "op" with
   | "login" ->
     event_login_login db
-      (cgi#argument_value "user")
-      (cgi#argument_value "pass")
+      (get_user cgi "user")
+      (get_pass cgi "pass")
       cgi
   | "forgot" ->
     event_login_forgot db (cgi#argument_value "user")
@@ -61,9 +132,7 @@ let event_admin_delete db session confirm =
     event_admin_logout db session
   end
 
-let event_admin_create db session user pass email admin =
-  let altemail = if email = "" then None else Some email
-  and level = if admin then Model.Admin else Model.User in
+let event_admin_create db session user pass altemail level =
   if pass = "" then
     let token = Model.user_create_nopw db session ~user ~altemail ~level
     in match altemail with
@@ -91,32 +160,32 @@ let event_admin db (cgi: Netcgi.cgi) =
       event_admin_logout db session
     | "setpw" ->
       event_admin_change_password db session
-	(cgi#argument_value "pass")
-	(cgi#argument_value "pass2")
+	(get_pass cgi "pass")
+	(get_pass cgi "pass2")
     | "setmail" ->
       event_admin_change_email db session (cgi#argument_value "mail")
     | "delete" ->
       event_admin_delete db session (cgi#argument_exists "delete_confirm")
     | "create" ->
       event_admin_create db session
-	(cgi#argument_value "user")
-	(cgi#argument_value "pass")
-	(cgi#argument_value "mail")
-	(cgi#argument_exists "admin")
+	(get_user cgi "user")
+	(get_pass cgi "pass")
+	(get_mail_option cgi "mail")
+	(get_admin cgi "admin")
     | "massupdate" ->
       event_admin_mass_update db session cgi
     | _ ->
       view_admin db session []
 
 let event_forgot db (cgi: Netcgi.cgi) =
-  let user = cgi#argument_value "user"
-  and token = cgi#argument_value "token" in
+  let user = get_user cgi "user"
+  and token = get_nonempty_string cgi "token" in
   if cgi#argument_exists "pass" then
     match Model.session_from_token db ~user ~token with
     | Some auth ->
       event_admin_change_password_forgot db token auth
-	(cgi#argument_value "pass")
-	(cgi#argument_value "pass2")
+	(get_nonempty_string cgi "pass")
+	(get_nonempty_string cgi "pass2")
     | None ->
       view_login db LoginFailed
   else
