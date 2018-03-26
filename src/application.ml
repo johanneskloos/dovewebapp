c7f459faaf2ffbc07507b02506b1a046919905c8
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-26 21:14:44 +0100

    Make acquiring the DB handle global.

--- src/application.ml
+++ src/application.ml
@@ -1,20 +1,20 @@
+let db_handles = ref []
+
+let take_handle () =
+  match !db_handles with
+  | handle :: handles -> db_handles := handles; handle
+  | [] -> Database.connect Config.(get database)
+
+let drop_handle handle =
+  if List.length !db_handles > 3 then
+    ignore (Sqlite3.db_close handle.Database.handle)
+  else
+    db_handles := handle :: !db_handles
+
 module Make(E: ModelDatabase.Externals)(Mail: Mails.Strategy) = struct
   module M = ModelDatabase.Make(E)
   module V = ViewWeb.Make(M)(ViewCgi)
   module C = Controller.Make(M)(V)(Mail)
-
-  let db_handles = ref []
-
-  let take_handle () =
-    match !db_handles with
-    | handle :: handles -> db_handles := handles; handle
-    | [] -> Database.connect Config.(get database)
-
-  let drop_handle handle =
-    if List.length !db_handles > 3 then
-      ignore (Sqlite3.db_close handle.Database.handle)
-    else
-      db_handles := handle :: !db_handles
 
   let handle_request (cgi: Netcgi.cgi) =
     let model = take_handle ()
