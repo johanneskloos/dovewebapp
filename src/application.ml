be9a65f69487cab3eec47a954b88b5ea7ca02699
Johannes Kloos <jkloos@unix-ag.uni-kl.de>
2018-03-27 09:43:08 +0100

    Do not cache DB handles.
    This code breaks testability due to mis-sharing.

--- src/application.ml
+++ src/application.ml
@@ -1,15 +1,8 @@
-let db_handles = ref []
-
 let take_handle () =
-  match !db_handles with
-  | handle :: handles -> db_handles := handles; handle
-  | [] -> Database.connect Config.(get database)
+  Database.connect Config.(get database)
 
 let drop_handle handle =
-  if List.length !db_handles > 3 then
-    ignore (Sqlite3.db_close handle.Database.handle)
-  else
-    db_handles := handle :: !db_handles
+  ignore (Sqlite3.db_close handle.Database.handle)
 
 module Make(E: ModelDatabase.Externals)(Mail: Mails.Strategy) = struct
   module M = ModelDatabase.Make(E)
